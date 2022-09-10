30f6e1729cfd1b407719a2ba41c6386d3c47edd7
Raman Krytski <rkeydevelop@gmail.com>
2022-09-10 20:01:31 +0200

    feat: created app class for generate pages added imports

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,9 +1,30 @@
+import Page from "../../core/templates/page";
 import MainPage from "../main";
-
+import SettingsPage from "../settings";
+import StatisticsPage from "../statistics";
 
 class App {
     private container: HTMLElement;
     private initialPage: MainPage;
+
+    static renderPage(pageId: string) {
+        document.body.innerHTML = '';
+        let page: Page | null = null;
+
+        if(pageId === 'main-page') {
+            page = new MainPage(pageId);
+        }else if(pageId === 'settings-page') {
+            page = new SettingsPage(pageId);
+        }else if(pageId === 'statistics-page') {
+            page = new StatisticsPage(pageId);
+        }
+        
+
+        if(page) {
+            const pageHTML = page.render();
+            document.body.append(pageHTML);
+        }
+    }
 
     constructor() {
         this.container = document.body;
@@ -11,8 +32,9 @@ class App {
     }
 
     run(){
-        const mainPageHTML = this.initialPage.render()
-        this.container.append(mainPageHTML)
+        App.renderPage('main-page')
+        // const mainPageHTML = this.initialPage.render()
+        // this.container.append(mainPageHTML)
     }
 }
 
